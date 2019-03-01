256847414304b8e0029c4bf7b22fc964cb809aa5
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2019-03-01 14:40:29 +0100

    chore: ensure nginx work on a read-only container

--- dockerfiles/php-lol/entrypoint.sh
+++ dockerfiles/php-lol/entrypoint.sh
@@ -13,6 +13,10 @@ fi
 
 export TMPDIR
 
+mkdir -p /dev/shm/nginx/{client_temp,fastcgi_temp,proxy_temp,scgi_temp,uwsgi_temp}
+chown -R www-data.www-data /dev/shm/nginx
+chmod -R 07500 /dev/shm/nginx
+
 cd /
 
 exec "$@"
