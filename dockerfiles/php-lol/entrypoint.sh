08205ce8a035c0d8efe4c9fbef0276a0315c3d13
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2019-03-01 14:39:26 +0100

    chore: set TMPDIR to `/dev/shm` if container is read-only

--- dockerfiles/php-lol/entrypoint.sh
+++ dockerfiles/php-lol/entrypoint.sh
@@ -4,6 +4,15 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+TMPDIR='/tmp'
+
+# Set /dev/shm as TMP_DIR if container is read-only
+if [[ ! -w "$TMPDIR" ]]; then
+  TMPDIR='/dev/shm'
+fi
+
+export TMPDIR
+
 cd /
 
 exec "$@"
