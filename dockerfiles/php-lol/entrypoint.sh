218e68858ffc5ea984eee284b3288e5388563a41
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2019-04-19 13:53:11 +0200

    feat: graceful reload

--- dockerfiles/php-lol/entrypoint.sh
+++ dockerfiles/php-lol/entrypoint.sh
@@ -4,6 +4,13 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+function _reload() {
+  kill -HUP "$(cat /run/nginx.pid)"
+  kill -USR2 "$(cat /run/php-fpm.pid)"
+}
+
 cd /
+
+trap _reload SIGHUP
 
 exec "$@"
