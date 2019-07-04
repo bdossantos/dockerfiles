dfca0b9c67d3a17e9ce15fb71bca8fcbcd706328
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2019-07-04 18:57:23 +0200

    Revert "feat: graceful reload"
    This reverts commit 218e68858ffc5ea984eee284b3288e5388563a41.

--- dockerfiles/php-lol/entrypoint.sh
+++ dockerfiles/php-lol/entrypoint.sh
@@ -4,13 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-function _reload() {
-  kill -HUP "$(cat /run/nginx.pid)"
-  kill -USR2 "$(cat /run/php-fpm.pid)"
-}
-
 cd /
-
-trap _reload SIGHUP
 
 exec "$@"
