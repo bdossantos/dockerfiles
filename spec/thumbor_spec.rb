a327f75e3ef9367e254b7150dc2dbd4444d011c8
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2021-09-05 20:58:24 +0200

    tests: run `thumbor-doctor`

--- spec/thumbor_spec.rb
+++ spec/thumbor_spec.rb
@@ -42,4 +42,8 @@ describe 'Dockerfile' do
         '9aa3694a8991a57bc7ca8913fe7a0cb9ebbbf84287a92432bb6303218021204e'
     }
   end
+
+  describe command('/app/bin/thumbor-doctor') do
+    its(:exit_status) { should eq 0 }
+  end
 end
