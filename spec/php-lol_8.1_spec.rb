c2cadff4f3ff44702b9a9899ed557ddc3a3b9ecf
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2021-12-27 16:14:49 +0100

    chore: build php-lol 8.1 image

---
+++ spec/php-lol_8.1_spec.rb
@@ -0,0 +1,191 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'securerandom'
+
+describe 'Dockerfile' do
+  PHP_VERSION = File.basename(__FILE__)[/php-lol_(.*)_spec.rb/, 1]
+  image = Docker::Image.build_from_dir(
+    "#{DOCKERFILES}/php-lol/",
+    'dockerfile' => "Dockerfile.#{PHP_VERSION}"
+  )
+
+  set :os, family: :debian
+  set :backend, :docker
+  set :docker_image, image.id
+
+  describe command('/usr/local/sbin/php-fpm -t') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('/usr/sbin/nginx -t') do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should contain("nginx: the configuration file /etc/nginx/nginx.conf syntax is ok\n") }
+    its(:stderr) { should contain("nginx: configuration file /etc/nginx/nginx.conf test is successful\n") }
+  end
+
+  describe file('/usr/sbin/nginx') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 755 }
+    its(:sha256sum) {
+      should eq \
+        '996dddf9bd7b360f9d2e4f8457cd4a3fcaf0875ff4d549f79743a76fdb0a437d'
+    }
+  end
+
+  describe file('/usr/local/bin/php') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 755 }
+    its(:sha256sum) {
+      should eq \
+        '44d6ef30b9a4b6bd48a98c102158a144a643082482c90ddb5db48389430b706d'
+    }
+  end
+
+  describe file('/usr/local/sbin/php-fpm') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 755 }
+    its(:sha256sum) {
+      should eq \
+        'a076ef550fee7b4d33d978ad4f5788e7edd6e4157388e2846bc813be4cb835e1'
+    }
+  end
+
+  describe file('/usr/local/etc/php/php.ini') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        'bb473d5f1b07ae332bb69305d1e3eb20ef4f27537d20311a174f904000a9e3a5'
+    }
+  end
+
+  describe file('/usr/local/etc/php/conf.d/zzz-apcu.ini') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        'd89f0a34e79a7389c2522b77d5200e88262d1c5ccb926062cb56924520d4cc60'
+    }
+  end
+
+  describe file('/usr/local/etc/php/conf.d/zzz-opcache.ini') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        'e1137b3fe6413c11f9991ee422ad36d811d622b0eabdc055cc63871ad9bff52a'
+    }
+  end
+
+  describe file('/usr/local/etc/php-fpm.d/zzz-php-fpm-tuning.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        '2b360a296bea08660b69bddc14a0c9c25044ffd1db94465fb9dde784c59cc9c2'
+    }
+  end
+
+  describe file('/usr/local/etc/php/conf.d/zzz-php-hardening.ini') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        '489ba97dca079d84e7dc21afb68b0acf7954da8ad150279be70053dce7243f83'
+    }
+  end
+
+  describe file('/etc/nginx/nginx.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        'e41188f0861cc3c89cb034efe7b3d0ebefe1f405a0903b1a28f25072eada7a34'
+    }
+  end
+
+  describe file('/etc/nginx/mime.types') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        'ac27e1f587ecebc46ded3265479efdc6cb2f711c22e8221dadb00b7b4db55369'
+    }
+  end
+
+  describe file('/etc/supervisor/supervisord.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        '58c9ac10c9be083a55681f2b4166a92c22fee3a7c35157da9dd25716556dc4ee'
+    }
+  end
+
+  describe file('/etc/supervisor/conf.d/app.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_mode 444 }
+    its(:sha256sum) {
+      should eq \
+        'bbdd21d87a9b30770de6b434673caf6594eb67bc4ff5c124f8c0ccb453c62d6b'
+    }
+  end
+
+  # FIXME: install geoip
+  %w[
+    amqp
+    apcu
+    bcmath
+    bz2
+    exif
+    gd
+    iconv
+    igbinary
+    imap
+    imagick
+    intl
+    mbstring
+    memcached
+    pcntl
+    pdo_mysql
+    pdo_pgsql
+    pgsql
+    posix
+    redis
+    soap
+    sockets
+    sodium
+    tidy
+    zip
+  ].each do |extension|
+    describe command("php -m | grep -i #{extension}") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) {
+        should contain(extension)
+      }
+    end
+  end
+
+  %w[
+    GeoIP.dat
+    GeoIPv6.dat
+  ].each do |geoip|
+    describe file("/usr/share/GeoIP/#{geoip}") do
+      it { should be_file }
+      it { should be_mode 644 }
+    end
+  end
+end
